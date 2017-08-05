ad5b9c02d2cd9b480d987f7c876f2717f0acc274
kouki <kouworld0123@gmail.com>
2017-08-05 14:16:27 +0000

    blog/newから新規投稿できるように、rootpageに一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'blogs#index'
-  get 'blogs/index'
+  get '/blogs/index',    to: 'blogs#index'
+  get '/blogs/show/:id', to: 'blogs#show'
+  get '/blogs/new',      to: 'blogs#new'
+  post '/blogs',         to: 'blogs#create'
 end
